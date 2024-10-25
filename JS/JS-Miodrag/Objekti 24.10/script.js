9adb1a8786d9679375a6299ebeccf6a5ac497e36
shomiqs <shomiqs@gmail.com>
2024-10-25 14:22:34 +0200

    vezbe objects

--- JS/JS-Miodrag/Objekti 24.10/script.js
+++ JS/JS-Miodrag/Objekti 24.10/script.js
@@ -102,71 +102,154 @@
 // 3.Add a method that changes the type of cuisine to the given value.
 // 4.Add a method that delete a given ingredient from the list of ingredients.
 
-function culinaryRecipe(
-  name,
-  cuisine,
-  ingredients,
-  complexity,
-  prepTime,
-  prepInstruction
-) {
-  let obj = {};
+// function culinaryRecipe(
+//   name,
+//   cuisine,
+//   ingredients,
+//   complexity,
+//   prepTime,
+//   prepInstruction
+// ) {
+//   let obj = {};
 
-  obj.name = name;
-  obj.cuisine = cuisine;
-  obj.complexity = complexity;
-  obj.ingredients = ingredients;
-  obj.prepTime = prepTime;
-  obj.prepInstruction = prepInstruction;
+//   obj.name = name;
+//   obj.cuisine = cuisine;
+//   obj.complexity = complexity;
+//   obj.ingredients = ingredients;
+//   obj.prepTime = prepTime;
+//   obj.prepInstruction = prepInstruction;
 
-  //dodali smo novu metodu van zadatka koja dodaje extra ingredient
-  // ----------------------------------------------------------------
-  obj.addNewIngredient = function (newIng) {
-    if (obj.ingredients.includes("salt")) {
-      console.log("Salt is already added!");
-    } else {
-      obj.ingredients.push(newIng);
-    }
-  };
-  // ----------------------------------------------------------------
-  obj.allIngredients = function () {
-    return obj.ingredients;
-  };
-  // ----------------------------------------------------------------
+//   //dodali smo novu metodu van zadatka koja dodaje extra ingredient
+//   // ----------------------------------------------------------------
+//   obj.addNewIngredient = function (newIng) {
+//     if (obj.ingredients.includes("salt")) {
+//       console.log("Salt is already added!");
+//     } else {
+//       obj.ingredients.push(newIng);
+//     }
+//   };
+//   // ----------------------------------------------------------------
+//   obj.allIngredients = function () {
+//     return obj.ingredients;
+//   };
+//   // ----------------------------------------------------------------
 
-  obj.prepUnder15min = function () {
-    if (obj.prepTime > 15) {
-      return false;
-    }
-    return true;
-  };
-  // ----------------------------------------------------------------
-  obj.typeOfCuisine = function (newCuisine) {
-    obj.cuisine = newCuisine;
-    return obj.cuisine;
-  };
-  // ----------------------------------------------------------------
-  return obj;
-}
+//   obj.prepUnder15min = function () {
+//     if (obj.prepTime > 15) {
+//       return false;
+//     }
+//     return true;
+//   };
+//   // ----------------------------------------------------------------
+//   obj.typeOfCuisine = function (newCuisine) {
+//     obj.cuisine = newCuisine;
+//     return obj.cuisine;
+//   };
+//   // ----------------------------------------------------------------
+//   return obj;
+// }
 
-let recipe = culinaryRecipe(
-  "Pasta",
-  "Italian",
-  ["pasta", "vegetables", "olive oil"],
-  "Medium",
-  14,
-  "Cook pasta, and mix together."
-);
+// let recipe = culinaryRecipe(
+//   "Pasta",
+//   "Italian",
+//   ["pasta", "vegetables", "olive oil"],
+//   "Medium",
+//   14,
+//   "Cook pasta, and mix together."
+// );
 
-recipe.addNewIngredient("salt");
-recipe.addNewIngredient("salt");
-recipe.typeOfCuisine("French");
+// recipe.addNewIngredient("salt");
+// recipe.addNewIngredient("salt");
+// recipe.typeOfCuisine("French");
 
-let updatedIngredients = recipe.allIngredients();
-let prepTime = recipe.prepUnder15min();
-let cuisineType = recipe.cuisine;
+// let updatedIngredients = recipe.allIngredients();
+// let prepTime = recipe.prepUnder15min();
+// let cuisineType = recipe.cuisine;
 
-console.log(cuisineType);
-console.log(updatedIngredients);
-console.log(prepTime);
+// console.log(cuisineType);
+// console.log(updatedIngredients);
+// console.log(prepTime);
+
 //  ZAVRSI ZADATAK, DODAJ 4. METODU
+// ----------------------------------------------------------------------------------------------
+// WEEK 3 | DAY 5 | Objects - pass by value/reference | Objects.docx - tasks(zadaci)
+// TASK 1.
+// Create a function that takes an array of objects (groceries) which calculates the total
+// price and returns it as a number. A grocery object has a product, a quantity and a price,
+// for example:
+// {
+// "product": "Milk",
+// "quantity": 1,
+// "price": 1.50
+// }
+
+// let groceryList = [
+//   { product: "Milk", quantity: 1, price: 1.5 },
+//   { product: "Bread", quantity: 2, price: 2.0 },
+//   { product: "Eggs", quantity: 1, price: 3.0 },
+// ];
+
+// function calculateTotalPrice(groceries) {
+//   let total = 0;
+//   for (let i = 0; i < groceries.length; i++) {
+//     total += groceries[i].price * groceries[i].quantity; // za slucaj da ima vise nekog proizvoda * groceries[i].quantity
+//   }
+//   return total;
+// }
+
+// console.log(calculateTotalPrice(groceryList));
+
+// ----------------------------------------------------------------------------------------------
+// TASK 2.
+
+// You go to a jewelry shop and try to find the most expensive piece of jewelry. You write
+// down the name of each piece of jewelry and its price. Create a function that gets the
+// name of the piece of jewelry with the highest price and returns "The most expensive
+// one is the {name of jewelry piece}";.
+// Example:
+// mostExpensive([{name:’DiamondEarings’, price: 980}, {name:’Gold watch’, price:250},…])
+// - The most expensive one is the diamond Ring.
+
+// let jewelryPiece = [
+//   { name: "DiamondEarings", price: 980 },
+//   { name: "GoldWatch", price: 250 },
+//   { name: "DiamondRing", price: 999 },
+// ];
+
+// function findMostExpensive(jewelry) {
+//   let highestPrice;
+//   let theMostExpensive = "The most expensive one is:";
+
+//   for (let i = 0; i < jewelry.length; i++) {
+//     if (!highestPrice || jewelry[i].price > highestPrice.price) {
+//       highestPrice = jewelry[i];
+//     }
+//   }
+//   return `${theMostExpensive} ${highestPrice.name} with price of - ${highestPrice.price}`;
+// }
+// console.log(findMostExpensive(jewelryPiece));
+
+// ----------------------------------------------------------------------------------------------
+// TASK 3.
+
+// Given a word, create an object that stores the indexes of each letter in an array. Make
+// sure the letters are the keys. Make sure the letters are symbols. Make sure the indexes
+// are stored in an array and those arrays are values.
+// Examples:
+// mapLetters("dodo") ➞ { d: [0, 2], o: [1, 3] }
+// mapLetters("froggy") ➞ { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+// mapLetters("grapes") ➞ { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
+
+// function eachLetter(word) {
+//   let newWord = {};
+//   for (let i = 0; i < word.length; i++) {
+//     let element = word[i];
+//     if (!newWord[element]) {
+//       newWord[element] = [];
+//     }
+//     newWord[element].push(i);
+//   }
+//   return newWord;
+// }
+// console.log(eachLetter("dodo"));
+// ----------------------------------------------------------------------------------------------
