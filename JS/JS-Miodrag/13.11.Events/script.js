4b32118ca5fec34a092b5073a0a899bd55b7c074
shomiqs <shomiqs@gmail.com>
2024-11-13 11:30:46 +0100

    promene

--- JS/JS-Miodrag/13.11.Events/script.js
+++ JS/JS-Miodrag/13.11.Events/script.js
@@ -2,6 +2,7 @@
 
 const button = document.querySelector(".button");
 const buttonOff = document.querySelector(".buttonOff");
+const buttonOn = document.querySelector(".buttonOn");
 const container = document.querySelector(".container");
 
 function toggleBackground() {
@@ -12,6 +13,10 @@ button.addEventListener("click", toggleBackground);
 
 buttonOff.addEventListener("click", () => {
   button.removeEventListener("click", toggleBackground);
+});
+
+buttonOn.addEventListener("click", () => {
+  button.addEventListener("click", toggleBackground);
 });
 
 //--------------------------------------------------------
