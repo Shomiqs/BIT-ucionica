2c1c2fe1a450632593e4fc5a873d66220c446ef2
shomiqs <shomiqs@gmail.com>
2024-11-13 12:07:05 +0100

    ChatoRoom task

--- JS/JS-Miodrag/13.11.Events/Task.2.ChatRoom/Chatrooms.js
+++ JS/JS-Miodrag/13.11.Events/Task.2.ChatRoom/Chatrooms.js
@@ -1,3 +1,14 @@
 "use strict";
 
-const container = document.querySelector(".container")
+const chatContainer = document.querySelector(".chat-container");
+const inputField = document.querySelector("input");
+const button = document.querySelector(".button");
+
+button.onclick = () => {
+  // napravi element:
+  const dataP = document.createElement("p");
+  // dodaj content u taj element:
+  dataP.innerHTML = inputField.value;
+  // append element na chat:
+  chatContainer.append(dataP);
+};
