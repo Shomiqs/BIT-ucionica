9807348905981e498298c85fcad850b57430e30a
shomiqs <shomiqs@gmail.com>
2024-12-23 14:50:59 +0100

    last day

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Planing a project.
+// for (let i = 0; i < 4; i++) {
+//   let array = [];
+//   array.push(i);
+// }
+// console.log(i);
+
+// var greeting = "Hello!";
+// function sayHello() {
+//   console.log(this.greeting);
+// }
+// sayHello();
+
+// function add (a, b) {
+//     var a = 10;
+//     console.log(a + b);
+// }
+// add(12,3);
+
+// console.log([2,3,4] == [2,3,4]);
+
+console.log([2, 11, 3].map(e => e - 2));
